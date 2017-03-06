356e2619b6a62d9e2abf560fdf4d70a9430f69df
HamLam <lamx0031@umn.edu>
2017-03-06 13:39:52 -0600

    Update control_pileup.sh

--- template/control_pileup.sh
+++ template/control_pileup.sh
@@ -20,7 +20,8 @@ BOWTIE2_DB=bowtie2_db_value
 S_DB=seq_db
 ref=/panfs/roc/rissdb/genomes/Homo_sapiens/hg19_canonical/seq/hg19_canonical.fa
 
-bwacommand="/home/msistaff/lamx0031/my_software/BWA/bwa-0.7.15/bwa mem -M -t 24 $BWA_DB $c_S1_R1 $c_S1_R2 | samtools view -q 10 -bS - > c_bwa.bam"
+#bwacommand="/home/msistaff/lamx0031/my_software/BWA/bwa-0.7.15/bwa mem -M -t 24 $BWA_DB $c_S1_R1 $c_S1_R2 | samtools view -q 10 -bS - > c_bwa.bam"
+bwacommand="bwa mem -M -t 24 $BWA_DB $c_S1_R1 $c_S1_R2 | samtools view -q 10 -bS - > c_bwa.bam"
 btcommand="bowtie2 -p 24 -k 5 -x $BOWTIE2_DB -1 $c_S1_R1 -2 $c_S1_R2 | samtools view -q 10 -bS - > c_bowtie2.bam"
 
 echo ${bwacommand} > $WORKING_PATH/aligncommands
